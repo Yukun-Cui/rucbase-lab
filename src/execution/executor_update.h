100fde0d15f8ac5086a3d32b2ce93e8c15b666bf
1572161937 <1572161937@qq.com>
2023-12-02 13:34:23 +0800

    commit lab3（之前忘记传executor_update.hl了）

--- src/execution/executor_update.h
+++ src/execution/executor_update.h
@@ -38,7 +38,42 @@ class UpdateExecutor : public AbstractExecutor {
         context_ = context;
     }
     std::unique_ptr<RmRecord> Next() override {
-        
+        // Update each rid from record file and index file
+        for (auto& rid : rids_) {
+            auto rec = fh_->get_record(rid, context_);
+            for (auto& set_clause : set_clauses_) {
+                auto lhs_col = tab_.get_col(set_clause.lhs.col_name);
+                memcpy(rec->data + lhs_col->offset, set_clause.rhs.raw->data, lhs_col->len);
+            }
+            // Remove old entry from index
+            for (size_t i = 0; i < tab_.indexes.size(); ++i) {
+                auto& index = tab_.indexes[i];
+                auto ih =
+                    sm_manager_->ihs_.at(sm_manager_->get_ix_manager()->get_index_name(tab_name_, index.cols)).get();
+                char* key = new char[index.col_tot_len];
+                int offset = 0;
+                for (size_t j = 0; j < index.col_num; ++j) {
+                    memcpy(key + offset, rec->data + index.cols[j].offset, index.cols[j].len);
+                    offset += index.cols[j].len;
+                }
+                ih->delete_entry(key, context_->txn_);
+            }
+            // Update record in record file
+            fh_->update_record(rid, rec->data, context_);
+            // Insert new index into index
+            for (size_t i = 0; i < tab_.indexes.size(); ++i) {
+                auto& index = tab_.indexes[i];
+                auto ih =
+                    sm_manager_->ihs_.at(sm_manager_->get_ix_manager()->get_index_name(tab_name_, index.cols)).get();
+                char* key = new char[index.col_tot_len];
+                int offset = 0;
+                for (size_t j = 0; j < index.col_num; ++j) {
+                    memcpy(key + offset, rec->data + index.cols[j].offset, index.cols[j].len);
+                    offset += index.cols[j].len;
+                }
+                ih->insert_entry(key, rid, context_->txn_);
+            }
+        }
         return nullptr;
     }
 
